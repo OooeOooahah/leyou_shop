8d65fc86f3709b86d77816b31def1f8384a5454d
OooeOooahah <Mr_Xiao_Wu@outlook.com>
2019-12-26 22:48:36 +0800

    eurake & gateway configration

---
+++ leyou-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * created by on 2019-12-26 下午 10:46
+ *
+ * @author "吴"
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class LyApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class);
+    }
+}
---
+++ leyou-registry/src/main/java/com/leyou/LyRegistry.java
@@ -0,0 +1,18 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * created by on 2019-12-26 下午 10:07
+ *
+ * @author "吴"
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class);
+        }
+}
